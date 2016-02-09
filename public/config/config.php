b39c60a398fdfbf0ede57466627ca2868e772770
RiCON <RiCON@nowhere>
2016-02-09 03:03:48 +0000

    Only get streams from "live"

--- public/config/config.php
+++ public/config/config.php
@@ -5,7 +5,7 @@ if( !defined( 'DOCUMENT_ROOT' ) ) 	define( 'DOCUMENT_ROOT', './' );
 
 //paths
 if( !defined( 'STAT_XML_URL' ) ) 	define( 'STAT_XML_URL', 'http://ls.fsbn.eu/stats/' );
-if( !defined( 'STAT_XML_XPATH' ) ) 	define( 'STAT_XML_XPATH', 'server/application/live/stream' );
+if( !defined( 'STAT_XML_XPATH' ) ) 	define( 'STAT_XML_XPATH', 'server/application[name="live"]/live/stream' );
 
 //files
 if( !defined( 'STREAM_XML' ) ) 		define( 'STREAM_XML', 'streams.xml' );
